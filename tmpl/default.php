b28dc74bd6f33215a4a8e6838e75cf51eab29ad3
Tristan de Boer <info@tristandeboer.nl>
2018-03-09 11:40:09 +0100

    Added icons

--- tmpl/default.php
+++ tmpl/default.php
@@ -18,9 +18,11 @@ defined('_JEXEC') or die;
 		<div class="col-5-sm articlesfeed-title" style="background-color: <?PHP echo $color; ?>">
 			<div class="head-title">
 				<?php print($list[0]->title) ?>
-				<?php foreach($list[0]->tags->itemTags as $tag) {
-					echo '<i class="icon-'.$tag->path.'"></i>&nbsp;';
-				}?>
+				<div class="icons">
+					<?php foreach($list[0]->tags->itemTags as $tag) {
+						echo '<i class="icon icon-'.$tag->path.'"></i>';
+					}?>
+				</div>
 			</div>
 			<div class="sub-title">
 				<?php print($list[0]->introtext) ?>
@@ -35,8 +37,8 @@ defined('_JEXEC') or die;
 			<li itemscope itemtype="https://schema.org/Article" style="color: <?PHP echo $color; ?>">
 				<a class="article-link" href="<?php echo $item->link; ?>" itemprop="url">
 					<span itemprop="name">
-						<?php echo $item->title; ?>&nbsp;<?php foreach($item->tags->itemTags as $tag) {
-							echo '<i class="icon-'.$tag->path.'"></i>&nbsp;';
+						<?php echo $item->title; ?><?php foreach($item->tags->itemTags as $tag) {
+							echo '<i class="icon icon-'.$tag->path.'"></i>';
 						}?>
 					</span>
 				</a>
